01eab0c6d06d0f7ef8569d36e71bf1920daa5ed7
fedDesilets <federicodesilets@gmail.com>
2022-07-28 21:46:40 -0400

    add the problem at hand for codewars exercise "find the next perfect square"

--- findNextPerfectSquare.js
+++ findNextPerfectSquare.js
@@ -1,3 +1,7 @@
+//create a function that takes a number and returns the next perfect square. if the number received
+//is not a perfect square, return -1
+
+
 function findNextSquare(sq){
     return Math.sqrt(sq)%1 == 0 ? Math.floor(Math.sqrt(sq)+1)*Math.floor(Math.sqrt(sq)+1) : -1;
 }
